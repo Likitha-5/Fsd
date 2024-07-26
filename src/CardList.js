fe1e0691832e5240c28d810f44df8d3e6c3cd6b5
Likitha-5 <likithasrinu5@gmail.com>
2024-07-26 15:38:43 +0530

    some

---
+++ src/CardList.js
@@ -0,0 +1,26 @@
+import Card from "./Card"
+function CardList() {
+    //const name="DETAILS";
+    const college = "SVECW ";
+    const userObject = { branch: "AIDS ", year: "4" };
+    const users = ["LIKITHA", "HARSHI", "PRANATHI", "DIVYA", "DEEPU","LALLI"]
+    return (
+        <div className="container">
+            <div className="row">
+
+                    {
+                        users.map(
+                            (data, index) => (<Card key={index}
+                                //program={name}
+                                col={college}
+                                user={userObject}
+                                userFromArray={data} />)
+                        )
+
+
+                    }
+                    </div>
+                </div>
+    );
+}
+export default CardList;
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import CardList from './CardList';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<CardList/>);
